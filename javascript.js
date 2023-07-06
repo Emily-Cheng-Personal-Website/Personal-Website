a0028b7605c348b265566f4727ff43cd47b410b2
Emily Cheng <echeng0123@gmail.com>
2023-07-06 18:50:49 -0400

    added working javascript to index (home) page

---
+++ javascript.js
@@ -0,0 +1,8 @@
+const greeting = "Welcome to my personal website!";
+const visited = 1;
+function firstHello() {
+    for (let i = 0; i < visited; i++) {
+        alert(greeting);
+    }
+}
+firstHello();
